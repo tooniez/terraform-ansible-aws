420ea4008d6a44ba4d86bad13b094a829baa72b0
tooniez <tooni22@proton.me>
2024-08-27 10:43:01 +0930

    feat: add tf web module

---
+++ terraform/modules/web/main.tf
@@ -0,0 +1,40 @@
+resource "aws_instance" "web" {
+  ami           = "${var.ami}"
+  count         = var.instance_count
+  instance_type = "${var.instance_type}"
+  key_name      = "${var.key_name}"
+
+  vpc_security_group_ids = [
+    "${var.sg_ids}",
+    "${aws_security_group.web.id}",
+  ]
+
+  tags = {
+    Name  = "${var.env}_${format("${var.name}%02d", count.index+1)}"
+    Group = "${var.env}_${var.name}_cluster"
+  }
+
+  provisioner "remote-exec" {
+    inline = ["#Connected!"]
+
+    connection {
+      host        = self.public_ip
+      agent       = false
+      type        = "ssh"
+      user        = var.ssh_user
+      private_key = file(var.private_key_path)
+    }
+  }
+}
+
+resource "aws_security_group" "web" {
+  name        = "${var.env}_example_web_sg"
+  description = "Allow HTTP access"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ terraform/modules/web/variables.tf
@@ -0,0 +1,43 @@
+variable pub_key_path {
+  description = "Path to ssh public key used to create this key on AWS"
+}
+
+variable ssh_user {
+  description = "User used to log in to instance"
+}
+
+variable private_key_path {
+  description = "Path to the private key used to connect to instance"
+}
+
+variable env {
+  description = "Environment prefix"
+}
+
+variable ami {
+  description = "Instance AMI"
+}
+
+variable instance_type {
+  description = "instance type"
+  default     = "t2.micro"
+}
+
+variable sg_ids {
+  description = "List of security groups ids"
+}
+
+variable key_name {
+  description = "Name of ssh key to create"
+}
+
+variable "instance_count" {
+  description = "Number of instances to create"
+  type        = number
+  default     = 1
+}
+
+variable name {
+  description = "Server name"
+  default     = "web"
+}
