0b054e7203cebec158f005be50735dc436352926
tooniez <tooni22@proton.me>
2024-09-03 22:48:00 +0930

    feat: add s3 bucket

---
+++ terraform/modules/s3/main.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "main" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.env
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
---
+++ terraform/modules/s3/variables.tf
@@ -0,0 +1,8 @@
+variable env {
+  description = "Environment prefix"
+}
+
+variable bucket_name {
+  description = "Name of the S3 bucket"
+  default = "tooniez-dev-s3-bucket"
+}
